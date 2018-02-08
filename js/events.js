542abbec24c8c1a66de1f986b47c5986600f8740
Daniel Gestino Notario <dageno11@gmail.com>
2018-02-08 03:31:31 +0100

    Add files via upload

---
+++ js/events.js
@@ -0,0 +1,85 @@
+let masterMind = new MasterMind();
+window.onload = masterMind.init;
+
+var arrayPelotas = [undefined, undefined, undefined, undefined];
+let comprobarArray = function(){
+    if(!arrayPelotas.includes(undefined)){
+        document.getElementById("check").setAttribute("class", "checkIN");
+        document.getElementById("check").setAttribute("style", "pointer-events: ;");
+        document.getElementById("check").addEventListener('click', masterMind.comprobar);
+        document.getElementById('main').scrollIntoView();
+    }else{
+        document.getElementById("check").setAttribute("class", "checkOUT");
+        document.getElementById("check").setAttribute("style", "pointer-events: none;");
+    }
+        
+}
+let pintar = function(){
+    let color =  this.className.split(" ")[1];
+    let arrayPrueba = $currentDiv.getElementsByClassName('circulo');
+
+    for(let i = 0; i<currentDiv.getElementsByClassName("circulo").length;i++){
+        if(arrayPrueba[i].getAttribute("name") != "coloreado"){
+            arrayPrueba[i].className += " "+color;
+            arrayPrueba[i].setAttribute('name', 'coloreado');
+            arrayPelotas[i] = color;
+            comprobarArray();   
+            return 0;
+        }
+    }
+   
+}
+let borrar = function(index){
+  arrayPelotas[index] = undefined;
+  currentDiv.getElementsByClassName('circulo')[index].className = "circulo";
+  currentDiv.getElementsByClassName('circulo')[index].setAttribute("name", "prueba")
+
+  console.log(arrayPelotas);
+  
+  comprobarArray();  
+  
+   
+}
+let crearLinea = function(){
+    let div = document.createElement('div');
+    let dd = document.createElement('dd');
+    let circulo;
+    let check;
+
+    div.setAttribute("class", "container");
+   
+
+    
+    for(let i = 0; i<4;i++){
+        circulo = document.createElement('div');
+        circulo.setAttribute("class", "circulo");
+        circulo.setAttribute("name", "prueba");
+        div.appendChild(circulo);
+    }
+    div.appendChild(dd);
+    for(let i = 0; i<4;i++){
+        check = document.createElement('div');
+        check.setAttribute("class", "check gris");
+        div.appendChild(check);
+    } 
+    document.getElementById('main').appendChild(div);
+
+}
+
+let asignarEventos = function(){
+    document.getElementsByName('prueba').forEach(function(element, index){
+        element.addEventListener('click', borrar.bind(this, index));
+    });
+    
+}
+
+for(let i = 0; i<document.getElementsByName('menu').length;i++){
+    document.getElementsByName('menu')[i].addEventListener('click', pintar);
+}
+
+
+
+
+
+
+    
---
+++ js/main.js
@@ -0,0 +1,90 @@
+
+     let currentDiv = document.getElementById('main').lastElementChild;
+
+    let MasterMind = function(){
+
+        let colores = ["negro", "azul", "verde", "amarillo", "rojo", "naranja", "blanco", "marron"];
+        let arrSolucion = new Array();
+       
+        var element = document.getElementsByClassName('container')[0];
+        var nuevo = element.cloneNode(true);
+        
+        
+
+        let numeroAleatorio = () => {
+            return Math.floor(Math.random()*colores.length);
+        }
+
+        let init = function(){
+            
+            for(let i = 0; i<4;i++)
+                arrSolucion.push(colores[numeroAleatorio()])
+            asignarEventos();
+            console.log(arrSolucion);
+            
+            
+        }
+    
+        let comprobarCoincidencia = function (){
+           
+            let arrayD =currentDiv.getElementsByClassName('circulo');
+            let array = [];
+            let negros = 0;
+            for(let i = 0; i<arrayD.length;i++){
+                array[i] = arrayD[i].className.split(" ")[1];    
+            }
+            
+            let coincidencias = 0;
+            for(let i = 0; i < 4; i++){
+                if(arrSolucion[i]===array[i]){
+                    currentDiv.getElementsByClassName('check')[coincidencias].className = 'check negro';
+                    negros++;
+                    coincidencias++;
+                }
+                    
+            }
+            if(coincidencias != 4){
+                array.forEach(function(elemento, index){
+                    if(arrSolucion.indexOf(elemento) >=0 && index != arrSolucion.indexOf(elemento)){
+                        currentDiv.getElementsByClassName('check')[coincidencias].className = 'check blanco';
+                        coincidencias++;
+                    }
+                
+                });
+
+                var elementosDiv = Array.prototype.slice.call(currentDiv.getElementsByTagName('div'));
+                elementosDiv.forEach(function(element){
+                    element.setAttribute("style", "pointer-events: none;");
+                })
+
+                crearLinea();
+               
+                currentDiv = document.getElementById('main').lastElementChild;
+                coincidencias = 0;
+
+                arrayPelotas = [undefined, undefined, undefined, undefined];
+                comprobarArray();
+                asignarEventos();
+            }else{
+
+                alert("Has ganado!");
+
+            }
+            
+                
+            
+        
+            
+        }
+        
+
+        return{
+            init: init,
+            comprobar: comprobarCoincidencia
+        }
+
+    }
+
+ 
+   // window.onclick = masterMind.comprobar;
+    
